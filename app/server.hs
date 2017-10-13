5a149cc841091625e4854f075522363d9d3ce892
徐敏辉 <xuminhui189@gmail.com>
2017-10-14 02:39:04 +0800

    refactor

--- app/server.hs
+++ app/server.hs
@@ -2,28 +2,22 @@
 {-# LANGUAGE RecordWildCards   #-}
 module Main (main) where
 
-import           Conduit                   (Sink, await, liftIO, ($$), ($$+),
-                                            ($$+-), (=$=))
-import           Control.Applicative       ((<$>))
-import           Control.Concurrent        (forkIO)
-import           Control.Concurrent.Async  (race_)
-import           Control.Exception         (throwIO)
-import           Control.Monad             (forever, void)
-import           Data.ByteString           (ByteString)
-import qualified Data.ByteString.Char8     as C
-import           Data.Conduit              (catchC)
-import           Data.Conduit.Network      (appSink, appSockAddr, appSource,
-                                            clientSettings, runTCPClient,
-                                            runTCPServer, serverSettings)
-import           Data.Monoid               ((<>))
-import           Data.Streaming.Network    (bindPortUDP)
-import           GHC.IO.Handle             (BufferMode (NoBuffering),
-                                            hSetBuffering)
-import           GHC.IO.Handle.FD          (stdout)
-import           Network.Socket            hiding (recvFrom)
-import           Network.Socket.ByteString (recvFrom, sendAllTo)
+import           Conduit                  (Sink, await, liftIO, ($$), ($$+),
+                                           ($$+-), (=$=))
+import           Control.Concurrent.Async (race_)
+import           Control.Exception        (throwIO)
+import           Data.ByteString          (ByteString)
+import qualified Data.ByteString.Char8    as C
+import           Data.Conduit             (catchC)
+import           Data.Conduit.Network     (appSink, appSockAddr, appSource,
+                                           clientSettings, runTCPClient,
+                                           runTCPServer, serverSettings)
+import           Data.Monoid              ((<>))
+import           GHC.IO.Handle            (BufferMode (NoBuffering),
+                                           hSetBuffering)
+import           GHC.IO.Handle.FD         (stdout)
 
-import           Shadowsocks.Encrypt       (getEncDec)
+import           Shadowsocks.Encrypt      (getEncDec)
 import           Shadowsocks.Util
 
 initRemote :: (ByteString -> IO ByteString)
@@ -42,37 +36,18 @@ main = do
     Config{..} <- parseConfigOptions
     let localSettings = serverSettings serverPort "*"
     C.putStrLn $ "starting server at " <> C.pack (show serverPort)
+    let
+      tcpHandler = flip runTCPServer $ \client -> do
+          (encrypt, decrypt) <- getEncDec method password
+          (clientSource, (host, port)) <-
+              appSource client $$+
+                  initRemote decrypt `catchC` \e ->
+                      error $ show (e :: SSException) <> " from "
+                            <> showSockAddr (appSockAddr client)
+          let remoteSettings = clientSettings port host
+          C.putStrLn $ "connecting " <> host <> ":" <> C.pack (show port)
+          runTCPClient remoteSettings $ \appServer -> race_
+              (clientSource $$+- cryptConduit decrypt =$= appSink appServer)
+              (appSource appServer $$ cryptConduit encrypt =$= appSink client)
 
-    udpSocket <- bindPortUDP serverPort "*"
-    void $ forkIO $ forever $ do
-        (encRequest, sourceAddr) <- recvFrom udpSocket 65535
-        forkIO $ do
-            (encrypt, decrypt) <- getEncDec method password
-            request <- decrypt encRequest
-            let (_, destAddr, destPort, payload) =
-                    either (error . show . UnknownAddrType) id (unpackRequest request)
-            C.putStrLn $ "udp " <> destAddr <> ":" <> C.pack (show destPort)
-            AddrInfo{..} <- head <$>
-                getAddrInfo Nothing (Just $ C.unpack destAddr)
-                                    (Just $ show destPort)
-
-            remote <- socket addrFamily Datagram defaultProtocol
-            sendAllTo remote payload addrAddress
-            (packet', sockAddr) <- recvFrom remote 65535
-            let packed = packSockAddr sockAddr
-            packet <- encrypt $ packed <> packet'
-            sendAllTo udpSocket packet sourceAddr
-            close remote
-
-    runTCPServer localSettings $ \client -> do
-        (encrypt, decrypt) <- getEncDec method password
-        (clientSource, (host, port)) <-
-            appSource client $$+
-                initRemote decrypt `catchC` \e ->
-                    error $ show (e :: SSException) <> " from "
-                          <> showSockAddr (appSockAddr client)
-        let remoteSettings = clientSettings port host
-        C.putStrLn $ "connecting " <> host <> ":" <> C.pack (show port)
-        runTCPClient remoteSettings $ \appServer -> race_
-            (clientSource $$+- cryptConduit decrypt =$= appSink appServer)
-            (appSource appServer $$ cryptConduit encrypt =$= appSink client)
+    tcpHandler localSettings
