e355ad5406f0bf14f7aa639c1cbcf65785531c03
uuhan <xuminhui189@gmail.com>
2017-12-29 23:34:11 +0800

    recv 64 byte not hang? why?

--- app/server.hs
+++ app/server.hs
@@ -32,12 +32,12 @@ main = do
     C.putStrLn $ "starting server at " <> C.pack (show serverPort)
     serve "*" (show serverPort) $ \(client, _) -> do
       (encrypt, decrypt) <- getEncDec method password
-      runEffect ((Nothing <$ fromSocket client 4096) >-> initRemote decrypt) >>=
+      runEffect ((Nothing <$ fromSocket client 64) >-> initRemote decrypt) >>=
         \case Nothing -> error "connection closed unexpected"
               Just (dest, port) -> do
                 C.putStrLn $ "connecting " <> dest <> ":" <> C.pack (show port)
                 connect (C.unpack dest) (show port) $ \(server, _) -> do
-                  let forward = fromSocket client 4096 >-> cryptPipe decrypt >-> toSocket server
-                      back    = fromSocket server 4096 >-> cryptPipe encrypt >-> toSocket client
+                  let forward = fromSocket client 64 >-> cryptPipe decrypt >-> toSocket server
+                      back    = fromSocket server 64 >-> cryptPipe encrypt >-> toSocket client
                   -- hangs?
                   race_ (runEffect forward) (runEffect back)
